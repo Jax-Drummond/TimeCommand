d45c945c337bebf8104060a8a443c2e6837c5e11
Jax <jaxdrummond9@gmail.com>
2023-12-03 18:22:47 -0500

    Update: updated to TerminalApi's newest tech

--- TimeTerminalCommand/Plugin.cs
+++ TimeTerminalCommand/Plugin.cs
@@ -6,29 +6,18 @@ using static TerminalApi.TerminalApi;
 
 namespace TimeTerminalCommand
 {
-	[BepInPlugin("atomic.timecommand", "Time Command", "1.0.0")]
-	[BepInDependency("atomic.terminalapi")]
+	[BepInPlugin("atomic.timecommand", "Time Command", "1.0.1")]
+	[BepInDependency("atomic.terminalapi", MinimumDependencyVersion: "1.2.0")]
 	public class Plugin : BaseUnityPlugin
 	{
 		private void Awake()
 		{
 			Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
 			Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
-			AddKeywords();
-		}
 
-		public void AddKeywords()
-		{
-			TerminalNode triggerNode = CreateTerminalNode($"You're not on a moon. There is no time here.\n", true);
-			TerminalKeyword verbKeyword = CreateTerminalKeyword("check", true);
-			TerminalKeyword nounKeyword = CreateTerminalKeyword("time");
-
-			verbKeyword = verbKeyword.AddCompatibleNoun(nounKeyword, triggerNode);
-			nounKeyword.defaultVerb = verbKeyword;
-
-			AddTerminalKeyword(verbKeyword);
-			AddTerminalKeyword(nounKeyword);
-		}
+			// Adds time command, 'check' is the verb. Verbs are optional
+            AddCommand("time", "You're not on a moon. There is no time here.\n", "check", true);
+        }
 
 		public static void UpdateKeywords()
 		{
